a8add488ab529a03415e0ba28d1df1ad75713a89
Kevin <kevin@invato.nl>
2024-04-11 01:12:06 +0200

    tree fix

--- components/RenderMenu.php
+++ components/RenderMenu.php
@@ -64,127 +64,118 @@ class RenderMenu extends ComponentBase
     }
 
     private function getMenuItems()
-    {
-        $menu = $this->menu;
-        
-        if ( !$menu ) {
+    {        
+        if ( !$this->menu ) {
             return;
         }
 
-        $menu_id = $menu->id;
+        $menu_id = $this->menu->id;
         $menuItems = MenuItem::where('menu_id', $menu_id)->where('is_hidden', false)->get();
         $items = [];
 
         foreach ($menuItems->toNested() as $item) {
             if ( $item->url ) {
                 $page = $this->resolvePage($item->url);
-                $isActive = $page->isActive;
-                $url = $page->url;
-            } else {
-                $url = null;
-                $isActive = false;
             }
-            $menuitem = [];
             
             if ( $item->replace_with_nested ) {
-                if ( $page->items ) {
-                    foreach ( $page->items as $nested ) {
-                        $menuitem['label'] = $nested->title;
-                        $menuitem['url'] = $nested->url;
-                        $menuitem['is_external'] = false;
-                        $menuitem['css_class'] = null;
-                        $menuitem['code'] = null;
-                        $menuitem['custom_attributes'] = null;
-                        $menuitem['isActive'] = $nested->isActive;
-                        $menuitem['childActive'] = false;
-                        $menuitem['items'] = [];
-                        $subitem = [];
-                        
-                        if ( $item->include_nested ) {
-                            if ( $nested->items ) {
-                                foreach ( $nested->items as $subitem ) {
-                                    $subitem['label'] = $subitem->title;
-                                    $subitem['url'] = $subitem->url;
-                                    $subitem['is_external'] = false;
-                                    $subitem['css_class'] = null;
-                                    $subitem['code'] = null;
-                                    $subitem['custom_attributes'] = null;
-                                    $subitem['isActive'] = $subitem->isActive;
-                                    if ( $subitem->isActive ) {
-                                        $menuitem['childActive'] = true;
-                                    }
-                                    
-                                    $menuitem['items'][] = $subitem;
-                                }
-                            }
-                        }
-                        
-                        $items[] = $menuitem;
-                    }
-                }
-            }
-            
-            else {
-                $menuitem['label'] = $item->label;
-                $menuitem['url'] = $url;
-                $menuitem['is_external'] = $item->is_external;
-                $menuitem['css_class'] = $item->css_class;
-                $menuitem['code'] = $item->code;
-                $menuitem['custom_attributes'] = $item->custom_attributes;
-                $menuitem['isActive'] = $isActive;
-                $menuitem['childActive'] = false;
-                $menuitem['items'] = [];
-
-                if ( $item->include_nested ) {
-                    if ( $page->items ) {
-                        foreach ( $page->items as $nested ) {
-                            $subitem['label'] = $nested->title;
-                            $subitem['url'] = $nested->url;
-                            $subitem['is_external'] = false;
-                            $subitem['css_class'] = null;
-                            $subitem['code'] = null;
-                            $subitem['custom_attributes'] = null;
-                            $subitem['isActive'] = $nested->isActive;
-                            if ( $nested->isActive ) {
-                                $menuitem['childActive'] = true;
-                            }
-                            
-                            $menuitem['items'][] = $subitem;
-                        }
-                    }
+                if ( !$page->items ) {
+                    continue;
                 }
 
-                if ( $item->children ) {
-                    foreach ( $item->children as $child ) {
-                        $resolvedChild = $this->resolvePage($child->url);
-    
-                        if ( $child->replace_with_nested ) {
-                            $subitem['label'] = '';
-                            $subitem['url'] = '';
-                        } else {
-                            $subitem['label'] = $child->label;
-                            $subitem['url'] = $resolvedChild->url;
-                        }
-            
-                        $subitem['is_external'] = $child->is_external;
-                        $subitem['css_class'] = $child->css_class;
-                        $subitem['code'] = $child->code;
-                        $subitem['custom_attributes'] = $child->custom_attributes;
-                        $subitem['isActive'] = $this->isActive($child->url);
-    
-                        if ( $this->isActive($child->url) ) {
-                            $menuitem['childActive'] = true;
-                        }
-    
-                        $menuitem['items'][] = $subitem;
-                    }
+                foreach ( $page->items as $nestedItem ) {
+                    $items[] = $this->makeNestedMenuItem($item, $nestedItem);
                 }
-    
-                $items[] = $menuitem;
+            } else {
+                $items[] = $this->makeMenuItem($item);
             }
         }
+
         $menutree = collect($items);
         return $menutree;
+    }
+    
+    private function makeMenuItem($item, $parent = false)
+    {
+        $url = '';
+        $isActive = false;
+
+        if ( $item->url ) {
+            $page = $this->resolvePage($item->url);
+            $isActive = $page->isActive;
+            $url = $page->url;
+        }
+
+        $menuitem = [];
+
+        $menuitem['label'] = $item->label;
+        $menuitem['url'] = $url;
+        $menuitem['is_external'] = $item->is_external;
+        $menuitem['css_class'] = $item->css_class;
+        $menuitem['code'] = $item->code;
+        $menuitem['custom_attributes'] = $item->custom_attributes;
+        $menuitem['isActive'] = $isActive;
+        $menuitem['childActive'] = false;
+        $menuitem['items'] = [];
+
+        if ( $item->include_nested ) {
+            if ( $page->items ) {
+                foreach ( $page->items as $nested ) {
+                    $menuitem['items'][] = $this->makeNestedMenuItem($item, $nested);
+                    if ( $nested->isActive ) {
+                        $menuitem['childActive'] = true;
+                    }
+                }
+            }
+        }
+
+        if ( !$item->children ) {
+            return $menuitem;
+        }
+
+        foreach ( $item->children as $child ) {
+            if ( $child->replace_with_nested ) {
+                $resolveChild = $this->resolvePage($child->url);
+                foreach ( $resolveChild->items as $nested ){
+                    $menuitem['items'][] = $this->makeNestedMenuItem($item, $nested);
+                    if ( $nested->isActive ) {
+                        $menuitem['childActive'] = true;
+                    }
+                }
+            } else {
+                $menuitem['items'][] = $this->makeMenuItem($child, $item);
+                $isActive = $this->isActive($child->url);
+                if ( $isActive ) {
+                    $menuitem['childActive'] = true;
+                }
+            }
+        }
+
+        return $menuitem;
+
+    }
+
+    private function makeNestedMenuItem($item, $nestedItem)
+    {
+        $menuitem = [];
+        $menuitem['label'] = $nestedItem->title;
+        $menuitem['url'] = $nestedItem->url;
+        $menuitem['isActive'] = $nestedItem->isActive;
+        $menuitem['childActive'] = false;
+
+        if ( !$item->include_nested ) {
+            return $menuitem;
+        }
+        
+        $menuitem['items'] = [];
+
+        if ( $nestedItem->items ) {
+            foreach ( $nestedItem->items as $subitem ) {
+                $menuitem['items'][] = $this->makeNestedMenuItem($item, $subitem);
+            }
+        }
+
+        return $menuitem;
     }
 
     public function getMenuCodeOptions()
